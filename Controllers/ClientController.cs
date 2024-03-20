e8b1011df190b7144ae2e9cc9df994ac58401681
EdisonP <133362311+edions@users.noreply.github.com>
2024-03-21 06:11:05 +0800

    Feature: Update client info

--- Controllers/ClientController.cs
+++ Controllers/ClientController.cs
@@ -43,6 +43,32 @@ namespace dotnet_web_mvc.Controllers
         }
 
         [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var client = _context.Clientinfos.FirstOrDefault(q => q.Id == id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            var userTypes = _context.Usertypes.ToList();
+            var selectedUserType = client.UserType;
+
+            ViewData["UserTypes"] = userTypes;
+            ViewData["SelectedUserType"] = selectedUserType;
+
+            return View(client);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Clientinfo c)
+        {
+            _context.Clientinfos.Update(c);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
         public IActionResult Delete(int id)
         {
             var client = _context.Clientinfos.Where(q => q.Id == id).FirstOrDefault();
