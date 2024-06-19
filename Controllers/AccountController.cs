986a80b38fc5ffa9d46bc969f0ec328b5026d80f
EdisonP <133362311+edions@users.noreply.github.com>
2024-06-19 09:01:00 +0800

    auth unfinished

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DinoLoan.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
